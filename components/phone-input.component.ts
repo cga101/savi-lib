ccb874372b67e8204f6148dbbd1ca7f127a86638
Alexander, Christian (ca434m) <ca434m@us.att.com>
2017-06-01 14:39:49 -0400

    Continuing to fix my branch

--- components/phone-input.component.ts
+++ components/phone-input.component.ts
@@ -8,12 +8,9 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 export class PhoneInput {
 	@Input() idsuffix;
 	@Input() pn;
-<<<<<<< HEAD
-	public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
-=======
 
 	public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
->>>>>>> 83e4a87ca5edd35b84ccf922181e3a56bdd24591
+
 	private suffix: string;
 	@Output() mobileValueChanged = new EventEmitter();
 	@Output() mobileValueBlur = new EventEmitter();
